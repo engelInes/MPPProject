db1986e51a5bab0b6874900402d4083c4b4e9961
inesEngel <ines.engel@stud.ubbcluj.ro>
2024-03-26 11:36:53 +0200

    finished the chart page

--- src/pages/Chart Page/ChartPage.tsx
+++ src/pages/Chart Page/ChartPage.tsx
@@ -1,24 +1,43 @@
-import React, {PureComponent, useContext} from 'react';
-import {PieChart, Pie, Sector, Cell, ResponsiveContainer} from 'recharts';
+// import React, {PureComponent, useContext} from 'react';
+// import {PieChart, Pie, Sector, Cell, ResponsiveContainer} from 'recharts';
 
+import React, {useContext} from 'react';
+import {PieChart, Pie, Cell, ResponsiveContainer} from 'recharts';
+import {DataGrid} from '@mui/x-data-grid';
+import {useTheme} from '@mui/material/styles';
 import { BooksContext } from '../../context/BooksContext';
 import { Layout } from '../../components/layout/Layout';
+interface GenreCounts {
+    [genre: string]: number; // Define the index signature
+}
 export function ChartPage() {
     const data=useContext(BooksContext)?.books;
+    const genreCounts: GenreCounts = data.reduce((counts, book) => {
+        counts[book.getGenre()] = (counts[book.getGenre()] || 0) + 1;
+        return counts;
+    }, {} as GenreCounts);
+
+    // Convert genre counts to an array of objects
+    const chartData = Object.entries(genreCounts).map(([genre, count]) => ({
+        genre,
+        count,
+    }));
     return (
         <div className='chart-pie'>
             <Layout>
-                <PieChart width={1400} height={400}>
+                <PieChart width={1300} height={500}>
                     <Pie
-                        dataKey='id'
-                        startAngle={180}
+                        //dataKey='id'
+                        dataKey='count'
+                        nameKey='genre'
+                        startAngle={360}
                         endAngle={0}
-                        data={data}
+                        data={chartData}
                         cx='50%'
                         cy='50%'
-                        outerRadius={80}
+                        outerRadius={120}
                         fill='red'
-                        label
+                        label={({genre}) => genre}
                     />
                 </PieChart>
             </Layout>
